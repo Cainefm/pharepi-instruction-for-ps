4534fda34260b00ac9665a69e03fca27b71442aa
fanmin <Cainefm@gmail.com>
2024-07-16 15:32:24 +0800

    init git

---
+++ R codes for baseline PS instruction.R
@@ -0,0 +1,35 @@
+## Script name: R codes for baseline PS instruction.R
+## Purpose of script: The codes in the manuscript introcudtion PS methods
+## Author: Franco, FAN Min
+## Date Created: 2024-07-16
+## Copyright (c) FAN Min, 2024
+## Email: minfan@connect.hku.hk
+##
+## Notes:
+##
+##
+
+
+
+
+# Package loading and environment setting ----------------------------------
+options(scipen = 6, digits = 4)
+memory.limit(30000000)
+
+library(data.table)
+# source("functions/packages.R")       # loads up all the packages we need
+
+
+# regression adjustment ---------------------------------------------------
+
+
+# matching ----------------------------------------------------------------
+
+
+# stratification ----------------------------------------------------------
+
+
+
+# weighting ---------------------------------------------------------------
+
+
